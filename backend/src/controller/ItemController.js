314b613bfb75c4ccd3ea5c97df0a4358e7b2cc1a
claudiajerome07 <claudiajerome07@gmail.com>
2025-03-13 17:11:14 +0530

    solved the uniqueness problem

--- backend/src/controller/ItemController.js
+++ backend/src/controller/ItemController.js
@@ -1,74 +1,84 @@
-const Item=require('../Models/ItemModel.js')
+const Item = require('../Models/ItemModel.js')
 
-const getAllItem=async(req,res)=>{
-    try{
-        const item=await Item.find();
+const getAllItem = async (req, res) => {
+    try {
+        const item = await Item.find();
         return res.status(200).send(item)
-    }catch(err){
+    } catch (err) {
         return res.status(500).send({ Error: err.message })
-        
+
     }
 }
 
-const getItembyID=async(req,res)=>{
-    try{
-        const {id} = req.params.id
+const getItembyID = async (req, res) => {
+    try {
+        const { id } = req.params.id
         const item = await Item.findById(id)
         if (!item) {
-            return res.status(404).send({message: 'Item not found'})
+            return res.status(404).send({ message: 'Item not found' })
         }
         return res.status(200).json(item);
-    }catch(err){
+    } catch (err) {
         return res.status(500).send({ Error: err.message })
-        
+
     }
-    
+
 }
 
-const Lostitem=async(req,res)=>{
-    try{
-        const{ name, description, location, status}=req.body
+const Lostitem = async (req, res) => {
+    try {
+        const { name, description, location, status } = req.body
         if (!req.file) {
             return res.status(400).send({ message: 'Image upload failed' });
         }
-        const imageUrl=req.file.path
-        if(!name || !description || !location || !status || !imageUrl){
-            return res.status(400).send({message:'All the fields are required'})
+        const imageUrl = req.file.path
+        if (!name || !description || !location || !status || !imageUrl) {
+            return res.status(400).send({ message: 'All the fields are required' })
         }
-        const checkifitemAlreadyExists=await Item.findOne({name})
-        if(checkifitemAlreadyExists){
-            return res.status(400).send({message:'Item already registered'})
-        }
-        const item=await Item.create({name,description,location,status,imagePath:imageUrl,userId:req.user._id})
-        res.status(201).send({message:'Lost item reported successfully',item})
-        
-    }catch(err){
-        return res.status(500).send({ Error: err.message })
-        
-    }
-}
-
-const foundItem=async(req,res)=>{
-    try{
-        const {name,description,location}=req.body
-        if (!req.file) {
-            return res.status(400).send({ message: 'Image upload failed' });
-        }
-        const imageUrl=req.file.path
-        if(!name || !description || !location || !imageUrl){
-            return res.status(400).send({message:'All the fields are required'})
-        }
-        const checkifitemAlreadyExists = await Item.findOne({ name })
+        const checkifitemAlreadyExists = await Item.findOne({
+            name,
+            location,
+            userId: req.user._id,
+            status: "lost"
+        })
         if (checkifitemAlreadyExists) {
             return res.status(400).send({ message: 'Item already registered' })
         }
-        const item=await Item.create({name,description,location,status:'found',imagePath:imageUrl,userId:req.user._id})
-        return res.status(201).json({message:'Found Item reported successfully',item})
-        
-    }catch(err){
+        const item = await Item.create({ name, description, location, status, imagePath: imageUrl, userId: req.user._id })
+        res.status(201).send({ message: 'Lost item reported successfully', item })
+
+    } catch (err) {
         return res.status(500).send({ Error: err.message })
 
     }
 }
 
-module.exports={getAllItem,getItembyID,Lostitem,foundItem}
+const foundItem = async (req, res) => {
+    try {
+        const { name, description, location } = req.body
+        if (!req.file) {
+            return res.status(400).send({ message: 'Image upload failed' });
+        }
+        const imageUrl = req.file.path
+        if (!name || !description || !location || !imageUrl) {
+            return res.status(400).send({ message: 'All the fields are required' })
+        }
+        const checkifitemAlreadyExists = await Item.findOne({
+            name,
+            location,
+            userId: req.user._id,
+            status: "found"
+        })
+        if (checkifitemAlreadyExists) {
+            return res.status(400).send({ message: 'Item already registered' })
+        }
+        const item = await Item.create({ name, description, location, status: 'found', imagePath: imageUrl, userId: req.user._id })
+        return res.status(201).json({ message: 'Found Item reported successfully', item })
+
+    } catch (err) {
+        return res.status(500).send({ Error: err.message })
+
+    }
+}
+
+module.exports = { getAllItem, getItembyID, Lostitem, foundItem }

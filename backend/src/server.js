048b81d14aa29f9f0a400c8408d4eeb95db87c17
claudiajerome07 <claudiajerome07@gmail.com>
2025-03-13 12:33:57 +0530

    connected to database to make api calls

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const express=require('express')
 const cookieParser=require('cookie-parser')
 const app=express()
 require('dotenv').config({ path: './src/config/.env' })
-const DBconnect=require('./Database/DB.js')
+const {DBconnect}=require('./Database/DB.js')
 
 
 const ItemRouter=require('./routes/ItemRoute.js')

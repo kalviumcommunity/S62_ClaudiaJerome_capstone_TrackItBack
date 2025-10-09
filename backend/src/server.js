a31f5b660d52adc2a778f96aecaba401ec1ad67a
claudiajerome07 <claudiajerome07@gmail.com>
2025-10-09 14:59:25 +0530

    debugging errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,12 +10,28 @@ const ClaimRouter = require('./routes/ClaimRoute.js')
 const UserRouter = require('./routes/UserRoute.js')
 
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://trackitbackweb.netlify.app"   // ✅ your Netlify frontend URL
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
+
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }))
 app.use('/claim', ClaimRouter)
 app.use('/user', UserRouter)
 app.use('/item', ItemRouter)
